9ac276db22a2368a09d896f1a6424a4d7840717a
Spencer Barclay <spencerhb11@gmail.com>
2020-04-01 22:03:03 -0700

    Fixing Test one

--- SightSign/UITests/UnitTest1.cs
+++ SightSign/UITests/UnitTest1.cs
@@ -82,7 +82,7 @@ namespace UITests
             loadButton.Click();
             Thread.Sleep(3000);
 
-            Console.WriteLine("Edit Button Text: " + editButton.GetAttribute("Radius"));
+            Console.WriteLine("Edit Button Text: " + editButton.GetAttribute("Radius").GetType());
             Console.WriteLine("Load Button Type: " + loadButton.GetType());
 
             Console.WriteLine("Load Button Text: " + loadButton.Text);
